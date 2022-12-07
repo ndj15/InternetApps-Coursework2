4ba8ca97a10be574b8f9241c9a9a00a21ea6edc1
ndj15 <nathan.dj@hotmail.com>
2022-12-07 23:27:09 +0000

    more form validation

--- SignUp.js
+++ SignUp.js
@@ -3,32 +3,42 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const passwordCheck = document.getElementById('passConfirm')
 const heading = document.getElementById('heading')
-const passConfirmLabel = document.getElementById('passConfirmLabel')
-
+const subBtn = document.getElementById('submitBtn')
+const error  = document.getElementById('error')
 
 
 function passCheck(){
     console.log(password.innerHTML.length)
+    if(password.value == ''){
+        var msg = 'Field Cannot be Left Blank'
+        msg.style.color ='blue'
+        passwordError.innerHTML = msg.style.color
+    }
 
-    if(password.value.length< 4){
+    if(password.value.length < 4){
         //if password length has to be more than xyz
-
         console.log('Your length of password is' + password.value.length)
-    }else{
-
-        console.log('fine')
     }
 
 
 }
 
 function passMatch(){
+    if(passwordCheck.value == ''){
+///cannot leave blank
+
+
+    }
+
     if(password.value != passwordCheck.value){
         console.log("Passwords do not match")
+
         passConfirmLabel.innerHTML = "Confirm Password PASSWORD DO NOT MATCH"
+        passConfirmLabel.style.color= 'red'
     }
     else{
-        passConfirmLabel.innerHTML = "Confirm Password PASSWORD MATCH"
+        passConfirmLabel.innerHTML = "Confirm Password "
+        passConfirmLabel.style.color = 'green'
 
     }
 
@@ -36,3 +46,42 @@ function passMatch(){
 
 }
 
+function emailCheck(){
+    if(email.value ==''){
+        //cannot leave blank
+    }
+
+
+}
+
+
+
+
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    //email check
+
+    var emailChar = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
+
+    if(emailChar.text(email.value)){//if true
+        console.log('okay email')
+
+
+        
+
+
+    }   else{
+
+        console.log('bad email')
+    }
+    //password check
+
+    // password match check
+
+
+
+
+
+
+})//add function for clicking on button for submit
