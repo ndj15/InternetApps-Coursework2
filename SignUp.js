4bde7bdbe7b5ee48d04d0c58b4630636919c86e2
ndj15 <nathan.dj@hotmail.com>
2022-12-07 22:15:53 +0000

    validation part 1

--- SignUp.js
+++ SignUp.js
@@ -2,18 +2,36 @@ const form = document.getElementById('signUpForm')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const passwordCheck = document.getElementById('passConfirm')
+const heading = document.getElementById('heading')
+const passConfirmLabel = document.getElementById('passConfirmLabel')
 
-password.innerHTML = 33
+
 
 function passCheck(){
-if(password.innerHTML.length < 9){
+    console.log(password.innerHTML.length)
+
+    if(password.value.length< 4){
+        //if password length has to be more than xyz
+
+        console.log('Your length of password is' + password.value.length)
+    }else{
+
+        console.log('fine')
+    }
 
 
-    console.log('i need longer')
-}else{
-
-    console.log('fine')
 }
+
+function passMatch(){
+    if(password.value != passwordCheck.value){
+        console.log("Passwords do not match")
+        passConfirmLabel.innerHTML = "Confirm Password PASSWORD DO NOT MATCH"
+    }
+    else{
+        passConfirmLabel.innerHTML = "Confirm Password PASSWORD MATCH"
+
+    }
+
 
 
 }
